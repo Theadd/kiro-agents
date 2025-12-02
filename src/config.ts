e85b18d9645c8b9c1e350e068b563d70c4b290e0
R. Beltran <rbeltran8000@gmail.com>
2025-12-02 11:54:16 +0100

    add default config.ts file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+
+export const substitutions = {
+  '{{{MODE_COMMANDS}}}': () => `MODE COMMANDS
+  /strict {state}   Control strict mode (on/off)`,
+  '{{{EXTRA_COMPATIBILITY}}}': () => ``,
+  '{{{INTEGRATION_ENHANCEMENTS}}}': () => ``
+}
